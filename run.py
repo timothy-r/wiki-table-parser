d6770206dbd45e59e52481258c9bb7ed7157f980
timothy-r <tim.rodger@gmail.com>
2013-05-01 23:34:01 +0100

    added a runner script

--- run.py
+++ run.py
@@ -1,5 +1,19 @@
 # python runner
+import sys
+from wiki_table_parser import WikiTableParser
+
+print sys.argv[1]
+
+name = sys.argv[1]
 
 # 1 read file from args[1] into a string
+f = open(name,'r')
+#print f
+
 # 2 use a WikiTableParser to read data from the string into object / array
+p = WikiTableParser();
+p.feed(f.read())
 # 3 write the data back out as json
+rows = p.getData()
+
+print rows
--- wiki_table_parser.py
+++ wiki_table_parser.py
@@ -17,14 +17,18 @@ class WikiTableParser(HTMLParser):
 
     def __init(self):
         self.data = []
-	
+
+    def getData(self):
+		return self.data
+
 	# if tag == table and state == 'none' attrs.class contains wikitable then state = 'table'
     def set_table_state(self, attrs, location):
-        for t in attrs:
-            if t[0] == 'class' and 0 == t[1].find('wikitable'):
-                self.state = 'table'
-                return
-        self.state = 'none'
+        if 'start' == location:
+            for t in attrs:
+            	if t[0] == 'class' and 0 == t[1].find('wikitable'):
+                	self.state = 'table'
+                	return
+        	self.state = 'none'
 
     def set_row_state(self, location):
         # if in a table then state = header-row otherwise
@@ -38,12 +42,12 @@ class WikiTableParser(HTMLParser):
                 self.state = 'data-row'
             else:
                 self.state = ''
-                print self.header_row                
+                #print self.header_row                
                 self.data.append(self.header_row)
                 self.header_row = []
         elif 'data-row' == self.state:
             if 'end' == location:
-                print self.data_row
+                #print self.data_row
                 self.data.append(self.data_row)
                 self.data_row = []
 
@@ -77,7 +81,7 @@ class WikiTableParser(HTMLParser):
 
     def handle_endtag(self, tag):
         if 'table' == tag:
-			self.set_table_state(attrs, 'end')
+			self.set_table_state('', 'end')
         elif 'tr' == tag:
             self.set_row_state('end')
         elif 'th' == tag:
@@ -93,5 +97,7 @@ class WikiTableParser(HTMLParser):
             self.current = self.current + '' + data
 
 # instantiate the parser and fed it some HTML
-parser = WikiTableParser()
-parser.feed('<table class="wikitable sortable" style="text-align: right"><tr><th>Rank<br /></th><th>Country</th><th>Total in km (sq mi)</th><th>Land in km (sq mi)</th><th>Water in km (sq mi)</th><th>&#160;% water</th><th class="unsortable">Notes</th></tr><tr><td><span style="display:none">7000100000000000000</span>1</td><td align="left"><span class="flagicon"><img alt="" src="//upload.wikimedia.org/wikipedia/en/thumb/f/f3/Flag_of_Russia.svg/22px-Flag_of_Russia.svg.png" width="22" height="15" class="thumbborder" srcset="//upload.wikimedia.org/wikipedia/en/thumb/f/f3/Flag_of_Russia.svg/33px-Flag_of_Russia.svg.png 1.5x, //upload.wikimedia.org/wikipedia/en/thumb/f/f3/Flag_of_Russia.svg/44px-Flag_of_Russia.svg.png 2x" />&#160;</span><a href="/wiki/Russia" title="Russia">Russia</a></td><td><span style="display:none">7007170982420000000</span>17,098,242<br />(6,601,668)</td><td><span style="display:none">7007163777420000000</span>16,377,742<br />(6,323,482)</td><td><span style="display:none">7005720500000000000</span>720,500<br />(278,200)</td><td>4.21</td><td align="left">Largest country in the world.</td></tr><tr>')
+#parser = WikiTableParser()
+# parser.feed('<table class="wikitable sortable" style="text-align: right"><tr><th>Rank<br /></th><th>Country</th><th>Total in km (sq mi)</th><th>Land in km (sq mi)</th><th>Water in km (sq mi)</th><th>&#160;% water</th><th class="unsortable">Notes</th></tr><tr><td><span style="display:none">7000100000000000000</span>1</td><td align="left"><span class="flagicon"><img alt="" src="//upload.wikimedia.org/wikipedia/en/thumb/f/f3/Flag_of_Russia.svg/22px-Flag_of_Russia.svg.png" width="22" height="15" class="thumbborder" srcset="//upload.wikimedia.org/wikipedia/en/thumb/f/f3/Flag_of_Russia.svg/33px-Flag_of_Russia.svg.png 1.5x, //upload.wikimedia.org/wikipedia/en/thumb/f/f3/Flag_of_Russia.svg/44px-Flag_of_Russia.svg.png 2x" />&#160;</span><a href="/wiki/Russia" title="Russia">Russia</a></td><td><span style="display:none">7007170982420000000</span>17,098,242<br />(6,601,668)</td><td><span style="display:none">7007163777420000000</span>16,377,742<br />(6,323,482)</td><td><span style="display:none">7005720500000000000</span>720,500<br />(278,200)</td><td>4.21</td><td align="left">Largest country in the world.</td></tr><tr>')
+
+#print parser.getData()
