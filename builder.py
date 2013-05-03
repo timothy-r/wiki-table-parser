e8343d136759d5629a20d72dcf72ef2602ebb43d
timothy-r <tim.rodger@gmail.com>
2013-05-03 09:48:05 +0100

    added a builder class

---
+++ builder.py
@@ -0,0 +1,17 @@
+# python class to build data from commands
+
+class Builder():
+
+    def __init(self):
+        self.data = []
+
+    def addHeader(self, name):
+        self.headers.append(name)
+
+    def addData(self, value):
+        self.row.append(value)
+
+    def getData(self):
+        return self.data
+
+
--- run.py
+++ run.py
@@ -2,18 +2,20 @@
 import sys
 from wiki_table_parser import WikiTableParser
 
+# 1 download a file from a url
 print sys.argv[1]
 
 name = sys.argv[1]
 
-# 1 read file from args[1] into a string
+# 2 read file from args[1] into a string
 f = open(name,'r')
 #print f
 
-# 2 use a WikiTableParser to read data from the string into object / array
+# 3 use a WikiTableParser to read data from the string into object / array
 p = WikiTableParser();
 p.feed(f.read())
-# 3 write the data back out as json
+
+# 4 write the data back out as json
 rows = p.getData()
 
 print rows
