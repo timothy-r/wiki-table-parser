96d192554e918b8d16f9b733654e067953a5bc69
timothy-r <tim.rodger@gmail.com>
2013-05-03 11:23:01 +0100

    added first test

--- test_parser.py
+++ test_parser.py
@@ -12,12 +12,20 @@ class TestParser(unittest.TestCase):
     def tearDown(self):
         """ remove test fixtures """
 
-    def test_parse(self):
+    def test_parse_missing_reads_no_data(self):
         """ test simple table parsing """
-        self.assertTrue(1)
         b = Builder()
         p = WikiTableParser(b)
+        html = self.get_missing_fixture()
+        p.feed(html)
+        # assert b.getData() has one empty list
+        product = b.getData()
+        self.assertTrue(1 == len(product))
+        self.assertTrue(0 == len(product[0]))
 
+    def get_missing_fixture(self):
+        """ return html that contains no table data """
+        return '<html></html>'
 
 
 # run that test thing
