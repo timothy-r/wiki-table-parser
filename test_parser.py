7c2931f7881b0d89f9df170912d7d6bca7f85fa9
timothy-r <tim.rodger@gmail.com>
2013-05-03 11:33:39 +0100

    more tests, exposed bg

--- test_parser.py
+++ test_parser.py
@@ -13,7 +13,7 @@ class TestParser(unittest.TestCase):
         """ remove test fixtures """
 
     def test_parse_missing_reads_no_data(self):
-        """ test simple table parsing """
+        """ test parsing no table """
         b = Builder()
         p = WikiTableParser(b)
         html = self.get_missing_fixture()
@@ -23,9 +23,62 @@ class TestParser(unittest.TestCase):
         self.assertTrue(1 == len(product))
         self.assertTrue(0 == len(product[0]))
 
+    def test_parse_single_header(self):
+        """ test simple table header parsing """
+        b = Builder()
+        p = WikiTableParser(b)
+        html = self.get_single_header_fixture()
+        p.feed(html)
+        # assert b.getData() has one header list with 1 item
+        product = b.getData()
+        self.assertTrue(1 == len(product))
+        self.assertTrue(1 == len(product[0]))
+        self.assertTrue('one' == product[0][0])
+
+    def test_multi_class_table(self):
+        """ test simple table header parsing """
+        b = Builder()
+        p = WikiTableParser(b)
+        html = self.get_multi_class_fixture()
+        p.feed(html)
+        # assert b.getData() has one header list with 1 item
+        product = b.getData()
+        print product
+        self.assertTrue(1 == len(product))
+        self.assertTrue(1 == len(product[0]))
+        self.assertTrue('one' == product[0][0])
+
+
+    def test_parse_multi_header(self):
+        """ test simple table header parsing """
+        b = Builder()
+        p = WikiTableParser(b)
+        html = self.get_multi_header_fixture()
+        p.feed(html)
+        # assert b.getData() has one header list with 1 item
+        product = b.getData()
+        self.assertTrue(1 == len(product))
+        self.assertTrue(3 == len(product[0]))
+        self.assertTrue('one' == product[0][0])
+        self.assertTrue('two' == product[0][1])
+        self.assertTrue('three' == product[0][2])
+
+
     def get_missing_fixture(self):
         """ return html that contains no table data """
         return '<html></html>'
+
+    def get_single_header_fixture(self):
+        """ return html that contains one header value """
+        return '<html><body><table class="wikitable"><tr><th>one</th></tr></table></body></html>'
+
+    def get_multi_header_fixture(self):
+        """ return html that contains one header value """
+        return '<html><body><table class="wikitable"><tr><th>one</th><th>two</th><th>three</th></tr></table></body></html>'
+
+    def get_multi_class_fixture(self):
+        """ return html that contains multiple table classes value """
+        return '<html><body><table class="ouch wikitable other"><tr><th>one</th></tr></table></body></html>'
 
 
 # run that test thing
