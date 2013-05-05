e73c853e38d66a8977be0b3e42692b49f7180682
timothy-r <tim.rodger@gmail.com>
2013-05-05 14:57:07 +0100

    updated parser tests for new builder product

--- test_parser.py
+++ test_parser.py
@@ -17,22 +17,10 @@ class TestParser(unittest.TestCase):
 
     def assertEmptyData(self, product):
         """ assert that the product list has empty headers and data """
-        self.assertEqual(1, len(product))
-        self.assertEqual(0, len(product[0]))
+        self.assertEqual(0, len(product))
 
-    def assertHeaderLength(self, product, length):
-        self.assertEqual(length, len(product[0]))
-        
-    def assertOneHeader(self, product, value):
-        self.assertEqual(1, len(product))
-        self.assertHeaderLength(product, 1)
-        self.assertHeaderValue(product, 0, value)
-
-    def assertHeaderValue(self, product, index, value):
-        self.assertEqual(value, product[0][index])
-
-    def assertDataValue(self, product, row, index, value):
-        self.assertEqual(value, product[row][index])
+    def assertDataValue(self, product, row, key, value):
+        self.assertEqual(value, product[row][key])
 
     def whenProductIsGenerated(self, html):
         b = Builder()
@@ -61,46 +49,27 @@ class TestParser(unittest.TestCase):
 
         self.assertEmptyData(self.product)
 
-    def testParseSingleHeader(self):
-        """ test simple table header parsing """
-        html = self.get_single_header_fixture()
-        self.whenProductIsGenerated(html)
-
-        self.assertOneHeader(self.product, 'one')
-
-    def testMultiClassTable(self):
-        """ test simple table header parsing """
-        html = self.get_multi_class_fixture()
-        self.whenProductIsGenerated(html)
-        self.assertOneHeader(self.product, 'one')
-
-    def testParseMultiHeader(self):
+    def testParseMultiHeadersReadsNoData(self):
         """ test simple table header parsing """
         html = self.get_multi_header_fixture()
         self.whenProductIsGenerated(html)
 
-        self.assertEqual(1, len(self.product))
-        self.assertHeaderLength(self.product, 3)
-        self.assertHeaderValue(self.product, 0, 'one')
-        self.assertHeaderValue(self.product, 1, 'two')
-        self.assertHeaderValue(self.product, 2, 'three')
+        self.assertEmptyData(self.product)
 
     def testParseTableData(self):
         """ test single row table data parsing """
         html = self.get_table_data_fixture()
         self.whenProductIsGenerated(html)
-        self.assertEqual(2, len(self.product))
-        self.assertHeaderValue(self.product, 0, 'one')
-        self.assertHeaderValue(self.product, 1, 'two')
-        self.assertDataValue(self.product, 1, 0, '1')
-        self.assertDataValue(self.product, 1, 1, 'England')
+        self.assertEqual(1, len(self.product))
+        self.assertDataValue(self.product, 0, 'one', '1')
+        self.assertDataValue(self.product, 0, 'two', 'England')
 
     def testIgnoreHiddenValues(self):
         """ test parser ignores data flagged as hidden """
         html = self.get_hidden_cell_data_fixture()
         self.whenProductIsGenerated(html)
 
-        self.assertDataValue(self.product, 1, 0, 'Show this')
+        self.assertDataValue(self.product, 0, 'one', 'Show this')
 
     def get_non_html(self):
         """ return a non-html string """
@@ -114,21 +83,13 @@ class TestParser(unittest.TestCase):
         """ return html that contains no table data """
         return '<html><body><table class="wrong"><tr><th>no</th></tr></table></body></html>'
 
-    def get_single_header_fixture(self):
-        """ return html that contains one header value """
-        return '<html><body><table class="wikitable"><tr><th>one</th></tr></table></body></html>'
-
     def get_multi_header_fixture(self):
         """ return html that contains one header value """
         return '<html><body><table class="wikitable"><tr><th>one</th><th>two</th><th>three</th></tr></table></body></html>'
 
-    def get_multi_class_fixture(self):
-        """ return html that contains multiple table classes value """
-        return '<html><body><table class="ouch wikitable other"><tr><th>one</th></tr></table></body></html>'
-
     def get_table_data_fixture(self):
         """ return html that contains table data """
-        return '<html><body><table class="wikitable"><tr><th>one</th><th>two</th></tr><tr><td>1</td><td>England</td></tr></table></body></html>'
+        return '<html><body><table class="not wikitable other"><tr><th>one</th><th>two</th></tr><tr><td>1</td><td>England</td></tr></table></body></html>'
 
     def get_hidden_cell_data_fixture(self):
         """ return html that contains hidden cell data """
